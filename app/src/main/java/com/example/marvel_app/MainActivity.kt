9519c1bc67bc803baee1c740338e545732a41aa3
Darth_neket <nikitacherenkov228@gmail.com>
2024-06-23 21:01:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.marvel_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.marvel_app.ui.theme.Marvel_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Marvel_appTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/marvel_app/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.marvel_app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackGround = Color(0xFF4E4E4E)
+val RedColor = Color(0xFFEC1D23)
+val GrayColor = Color(0xFF4E4E4E)
+val WhiteColor = Color.White
+val SearchColor = Color(0xFF363636)
+val SearchBorderColor = Color(0xFF696969)
+val SearchTextColor = Color(0xFF7C7C7C)
+
